ec53661a86f6c0814dd0dcf685357fc0b7f15328
ginoesco <g.escolano91@gmail.com>
2017-12-10 16:39:05 -0800

    erased console outputs for debugging

--- Tetris/Tetris/TetrisGame.cs
+++ Tetris/Tetris/TetrisGame.cs
@@ -256,7 +256,6 @@ namespace Tetris
             { //updates when up is pressed
                 List<int[,]> rotate = shapeObj.Rotate(currentShape);
 
-                Console.WriteLine("CurrentShape, RotateLength: {0}, {1}", currentShape, rotate.Count);
 
                 if ((currentShape == 6 || currentShape == 3 || currentShape == 4) && (moveLeftState >= 362 && moveRightState <= boundsX))
                 {
@@ -350,8 +349,6 @@ namespace Tetris
             newMouseState = Mouse.GetState();
             int blocked = (int)GameBoard.BlockStates.Blocked;
 
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
             if (j < 4 && blockstate != blocked)
             {
                 blockstate = (int)gbObj.CheckPlacement(loadedBoard, shape, xcoords[j], ycoords[j]);
@@ -461,6 +458,7 @@ namespace Tetris
                     }
                 case game:
                     Fall(levelobj.CalculateTimer(level));
+                    Console.WriteLine("Timer: {0}", levelobj.CalculateTimer(level));
                     if (currentKeyState != oldKeyState && currentKeyState.IsKeyDown(Keys.Home))
                     {
                         level++;
@@ -477,7 +475,7 @@ namespace Tetris
                         }
                     }
                     if ((pauseButton.update(new Vector2(newMouseState.X, newMouseState.Y)) == true && newMouseState != lastMouseState && newMouseState.LeftButton == ButtonState.Pressed)
-                            || (currentKeyState.IsKeyDown(Keys.LeftControl) && currentKeyState.IsKeyDown(Keys.P)))
+                            || (currentKeyState.IsKeyDown(Keys.LeftControl) && currentKeyState.IsKeyDown(Keys.P)) || (currentKeyState.IsKeyDown(Keys.Escape) && currentKeyState.IsKeyDown(Keys.Escape)))
                     {
                         currentScreen = pauseScreen;
                     }
